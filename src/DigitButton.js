1be9821ae9e4ad63f963362276d0109752a4513a
Alexandra <alexandraschach8282@outlook.de>
2023-07-25 21:08:35 +0200

    calculator commit

--- src/DigitButton.js
+++ src/DigitButton.js
@@ -1,5 +1,6 @@
 import { ACTIONS } from "./App";
 
+//DigitButton component represents a button that corresponds to a numeric digit.
 export default function DigitButton ({dispatch, digit}){
     return <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: {digit}})}
     >
